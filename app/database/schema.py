89ada42ed4ddac2fad5bdfc1ecfd6c560c820e9e
L-dongkyung <ckldk91m@naver.com>
2022-07-10 01:29:55 +0900

    [backend] 유저 생성 코드 작성(router, 오타 수정(schema.py), jwt)

--- app/database/schema.py
+++ app/database/schema.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 from sqlalchemy import Column, Integer, String, DateTime, func, Enum, Boolean
 from sqlalchemy.orm import Session
-from app.database.conn import Base
+from app.database.conn import Base, db
 
 class BaseMixin:
     id = Column(Integer, primary_key=True, index=True)
@@ -10,12 +10,13 @@ class BaseMixin:
     updated_at = Column(DateTime, nullable=False, default=func.utc_timestamp(), onupdate=func.utc_timestamp())
 
     def all_colums(self):
-        return [c for c in self.__table__.colums if c.primary_key is False and c.name != "created_at"]
+        return [c for c in self.__table__.columns if c.primary_key is False and c.name != "created_at"]
 
     def __hash__(self):
         return hash(self.id)
 
-    def create(self, session: Session, auto_commit: bool = False, **kwargs):
+    @classmethod
+    def create(cls, session: Session, auto_commit: bool = False, **kwargs):
         """
 
         :param session:
@@ -23,15 +24,33 @@ class BaseMixin:
         :param kwargs:
         :return:
         """
-        for col in self.all_colums():
+        obj = cls()
+        for col in obj.all_colums():
             col_name = col.name
             if col_name in kwargs:
-                setattr(self, col_name, kwargs.get(col_name))
-        session.add(self)
+                setattr(obj, col_name, kwargs.get(col_name))
+        session.add(obj)
         session.flush()
         if auto_commit:
             session.commit()
-        return self
+        return obj
+
+    @classmethod
+    def get(cls, **kwargs):
+        """
+        DB에서 정보(row)를 가져오는 함수.
+        :param kwargs:
+        :return:
+        """
+        session = next(db.session())
+        query = session.query(cls)
+        for key, value in kwargs.items():
+            col = getattr(cls, key)
+            query = query.filter(col == value)
+
+        if query.count() > 1:
+            raise Exception("하나의 데이터 리턴만 지원. 결과가 하나 이상입니다.")
+        return query.first()
 
 
 class Users(Base, BaseMixin):
--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import uvicorn
 
 from app.common.config import conf
 from app.database.conn import db
-from app.routes import index
+from app.routes import index, auth
 
 def create_app():
     """
@@ -16,15 +16,18 @@ def create_app():
     app = FastAPI()
     conf_dict = asdict(c)
     db.init_app(app, **conf_dict)
+    # Base.metadata.create_all(db.engine)
+
     # db initialize
     # redis initialize
     # middleware
     # router
     app.include_router(index.router)
+    app.include_router(auth.router, tags=['Authentication'])
 
     return app
 
 app = create_app()
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=conf().PROJ_RELOAD)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=conf().PROJ_RELOAD)
