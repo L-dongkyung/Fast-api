b1c26d7df99adb9b33cfbc24b2c4fcb6b6f82548
L-dongkyung <ckldk91m@naver.com>
2022-07-22 00:22:23 +0900

    [backend][middleware] Access-control 미들웨어에서 토큰 확인. /api로 시작하면 미들웨어에서 토큰 검증. /api로 시작 안하는데 쿠키에 토큰 없으면 에러.(익명 접속자 확인 필요.)

--- app/middlewares/token_validator.py
+++ app/middlewares/token_validator.py
@@ -1,3 +1,4 @@
+import re
 import time
 import typing
 import jwt
@@ -10,7 +11,7 @@ from starlette.datastructures import URL, Headers
 from starlette.responses import JSONResponse, PlainTextResponse, RedirectResponse, Response
 from starlette.types import ASGIApp, Receive, Send, Scope
 
-from app.common import config
+from app.common import config, consts
 from app.common.config import conf
 from app.models import UserToken
 
@@ -31,6 +32,13 @@ class AccessControl:
         self.except_path_regex = except_path_regex
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        """
+        미들웨어 토큰을 검증하는 곳
+        :param scope:
+        :param receive:
+        :param send:
+        :return:
+        """
         print(self.except_path_regex)
         print(self.except_path_list)
 
@@ -45,7 +53,42 @@ class AccessControl:
         request.state.inspect = None
         request.state.user = None
         request.state.is_admin_access = None
-        print(request.cookies)
-        print(headers)
-        res = await self.app(scope, receive, send)
-        return res
+        ip_from = request.headers['x-forwarded-for'] if "x-forwarded-for" in request.headers.keys() else None  # AWS ELB 통과 후 설정.
+
+        if await self.url_pattern_check(request.url.path, self.except_path_regex) or request.url.path in self.except_path_list:
+            return await self.app(scope, receive, send)
+
+        if request.url.path.startswith("/api"):
+            if "Authorization" in request.headers.keys():
+                request.state.user = await self.token_decode(access_token=request.headers.get("Authorization"))
+            else:
+                response = JSONResponse(status_code=401, content=dict(msg="AUTHORIZATION_REQUIRED"))
+                return await response(scope, receive, send)
+        else:
+            # 쿠키의 토큰을 받으려면 현 if구절을 주석하여 login 후 토큰을 이곳에 입력해야함.
+            request.cookies['Authorization'] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTAsImVtYWlsIjoidXNlckBleGFtcGxlLmNvbSIsIm5hbWUiOm51bGwsInBob25lX251bWJlciI6bnVsbH0.tJUIk7nWYIcUL5oWWPWbd-SHy4X9u6NWNxcneYLe1aE"
+            if "Authorization" not in request.cookies.keys():
+                response = JSONResponse(status_code=401, content=dict(msg="AUTHORIZATION_REQUIRED"))
+                return await response(scope, receive, send)
+            request.state.user = await self.token_decode(access_token=request.cookies.get("Authorization"))
+
+        request.state.req_time = Date.datetime()
+        return await self.app(scope, receive, send)
+
+    @staticmethod
+    async def url_pattern_check(path, pattern):
+        res = re.match(pattern, path)
+        if res:
+            return True
+        return False
+
+    @staticmethod
+    async def token_decode(access_token):
+        try:
+            access_token = access_token.replace("Bearer ", "")
+            payload = jwt.decode(access_token, key=consts.JWT_SECRET, algorithms=[consts.JWT_ALGORITHM])
+        except PyJWTError as e:
+            print(e)
+            raise
+            # 에러 발생 코드 추가 예정
+        return payload
--- app/routes/index.py
+++ app/routes/index.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from starlette.requests import Request
 from starlette.responses import Response
 
 from app.database.conn import db
@@ -26,3 +27,8 @@ async def index(session: Session = Depends(db.session)):
 
     current_time = datetime.utcnow()
     return Response(f"Fast-api check API (UTC: {current_time.strftime('%Y.%m.%d %H:%M:%S')})")
+
+@router.get("/test")
+async def test(request: Request):
+    current_time = datetime.utcnow()
+    return Response(f"test api Done (UTC: {current_time.strftime('%Y-%m-%d %H:%M:%S')})")
